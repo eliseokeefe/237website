8fa2428daff2c250435206900f9fa754eec38f4e
Elise Okeefe <e.m.okeefe@wustl.edu>
2025-04-17 13:42:09 -0500

    using 131 website

--- 237.js
+++ 237.js
@@ -1,51 +1,97 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loginButton = document.querySelector(".loginButton button");
+import React from "react";
 
-    loginButton.addEventListener("click", () => {
-        const clientId = "YOUR_CLIENT_ID"; //  GitHub OAuth app client ID
-        const redirectUri = "http://localhost:3000/callback"; // update w/ callback URL
-        const scope = "read:user user:email";
+class App extends React.Component {
+  handleGitHubLogin = () => {
+    const clientId = "CLIENT_ID"; // replace with GitHub OAuth client ID
+    const redirectUri = "http://localhost:3000/callback";
+    const scope = "read:user user:email";
+    // Update with backend 
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
+    window.location.href = githubAuthUrl;
+  };
 
-        const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scope)}`;
-
-        window.location.href = githubAuthUrl;
+  // setUserInfo definition 
+  setUserInfo = (
+    id,
+    username,
+    name,
+    userToken,
+    classes,
+    admin,
+    disableAudioAlerts
+  ) => {
+    this.setState({
+      id,
+      username,
+      name,
+      userToken,
+      tokenTime: Date.now(),
+      classes,
+      admin,
+      disableAudioAlerts,
     });
-});
+  };
 
+  // componentDidMount() declaration 
+  componentDidMount() {
+    if (window.location.pathname === "/callback") {
+      const urlParams = new URLSearchParams(window.location.search);
+      const code = urlParams.get("code");
 
-if (window.location.pathname === "/callback") {
-    const urlParams = new URLSearchParams(window.location.search);
-    const code = urlParams.get("code");
-
-    if (code) {
-        // Exchange code for an access token (do on backend)
+      if (code) {
         fetch("http://localhost:3000/exchange-token", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ code }),
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ code }),
         })
-            .then((response) => response.json())
-            .then((data) => {
-                const accessToken = data.access_token;
+          .then((res) => res.json())
+          .then((data) => {
+            const accessToken = data.access_token;
 
-                
-                return fetch("https://api.github.com/user", {
-                    headers: {
-                        Authorization: `Bearer ${accessToken}`,
-                    },
-                });
-            })
-            .then((response) => response.json())
-            .then((user) => {
-                // Save the username 
-                localStorage.setItem("username", user.login);
-                console.log("Logged in as:", user.login);
-            })
-            .catch((error) => {
-                console.error("Error during login:", error);
+            return fetch("https://api.github.com/user", {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
             });
+          })
+          .then((res) => res.json())
+          .then(async (user) => {
+            const username = user.login;
+
+            const response = await fetch("http://localhost:3000/user-info", {
+              method: "POST",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify({ username }),
+            });
+
+            const userData = await response.json();
+
+            // setUserInfo defined 
+            this.setUserInfo(
+              userData.id,
+              username,
+              userData.name,
+              userData.token,
+              userData.classes || [],
+              userData.admin || false,
+              userData.disableAudioAlerts || false
+            );
+          })
+          .catch((error) => {
+            console.error("GitHub login error:", error);
+          });
+      }
     }
+  }
+
+  // render with login button
+  render() {
+    return (
+      <div>
+        <h1>Welcome!</h1>
+      </div>
+    );
+  }
 }
-  
+
+export default App;
